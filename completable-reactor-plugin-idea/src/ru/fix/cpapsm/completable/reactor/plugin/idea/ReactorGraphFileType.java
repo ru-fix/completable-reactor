00c1d83ee48200a33d916cdce9358646a5e2b019
Kamil Asfandiyarov <kasfandiyarov@gmail.com>
2017-02-27 21:51:38 +0300

    Init reactor project

--- completable-reactor-plugin-idea/src/ru/fix/cpapsm/completable/reactor/plugin/idea/ReactorGraphFileType.java
+++ completable-reactor-plugin-idea/src/ru/fix/cpapsm/completable/reactor/plugin/idea/ReactorGraphFileType.java
@@ -6,7 +6,6 @@ import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import javax.swing.*;
 import java.nio.charset.StandardCharsets;
 
 /**
