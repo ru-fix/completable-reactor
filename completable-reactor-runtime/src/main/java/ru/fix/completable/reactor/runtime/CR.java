1176197fff5c23542e07f375fc095ed1823e5ec6
Kamil Asfandiyarov <kasfandiyarov@fix.ru>
2017-04-03 17:48:21 +0300

    Work on dsl interfaces in reactor.

--- completable-reactor-runtime/src/main/java/ru/fix/completable/reactor/runtime/CR.java
+++ completable-reactor-runtime/src/main/java/ru/fix/completable/reactor/runtime/CR.java
@@ -1,6 +1,7 @@
 package ru.fix.completable.reactor.runtime;
 
 import lombok.val;
+import ru.fix.completable.reactor.runtime.internal.CRReactorGraph;
 import ru.fix.completable.reactor.runtime.internal.ReactorReflector;
 
 import java.nio.charset.StandardCharsets;
@@ -14,12 +15,12 @@ public class CR {
     public void write(ReactorGraph<?>... graphs) throws Exception {
         for (ReactorGraph<?> graph : graphs) {
 
-
             val model = graph.serialize();
             model.serializationPointSource = ReactorReflector.getMethodInvocationPoint().orElse(null);
 
             val content = ReactorMarshaller.marshall(model);
-            val path = Paths.get(graph.getPayloadClass().getName() + ".rg");
+
+            val path = Paths.get(((CRReactorGraph)graph).getPayloadClass().getName() + ".rg");
             Files.write(path, content.getBytes(StandardCharsets.UTF_8));
         }
     }
