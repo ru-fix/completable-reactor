eda525f884cc0b20ea1ed3c358c9632b6bbdf170
kasfandiyarov <kasfandiyarov@fix.ru>
2018-01-22 13:01:59 +0300

    New Dsl Parser. Update EndPointNode

---
+++ completable-reactor-graph-viewer/src/main/kotlin/ru/fix/completable/reactor/graph/viewer/gl/EndPointNode.kt
@@ -0,0 +1,67 @@
+package ru.fix.completable.reactor.graph.viewer.gl
+
+import javafx.scene.control.ContextMenu
+import javafx.scene.control.Label
+import javafx.scene.control.MenuItem
+import javafx.scene.layout.VBox
+import javafx.scene.text.Font
+import ru.fix.completable.reactor.api.gl.model.*
+
+/**
+ * Created by swarmshine on 29.01.2017.
+ */
+class EndPointNode(
+        val translator: CoordinateTranslator,
+        val endPoint: EndPoint,
+        val actionListener: GraphViewer.ActionListener,
+        val positionListener: PositionListener) : VBox() {
+
+    init {
+
+        this.styleClass.add("endPoint")
+
+        this.layoutX = translator.translateX(endPoint.coordinates.x.toDouble())
+        this.layoutY = translator.translateY(endPoint.coordinates.y.toDouble())
+
+        val nameLabel = Label("End")
+        nameLabel.font = Font(16.0)
+
+        this.children.add(nameLabel)
+
+        this.setOnMouseClicked { mouseEvent ->
+            if (mouseEvent.clickCount == 2) {
+                endPoint.source?.let { actionListener.goToSource(it) }
+            }
+        }
+
+        val dragger = NodeDragger.attach(this)
+
+        dragger.addOnPositionChangedListener{
+            endPoint.coordinates.x = translator.reverseTranslateX(layoutX)
+            endPoint.coordinates.y = translator.reverseTranslateX(layoutY)
+            positionListener.positionChanged()
+        }
+
+        initializePopupMenu()
+
+        this.setOnMouseClicked { mouseEvent ->
+            if (mouseEvent.clickCount == 2) {
+                endPoint.source?.let { actionListener.goToSource(it) }
+                mouseEvent.consume()
+            }
+        }
+    }
+
+    private fun initializePopupMenu() {
+        val contextMenu = ContextMenu()
+        val menuItem = MenuItem("EndPoint")
+        contextMenu.items.add(menuItem)
+
+        menuItem.setOnAction { endPoint.source?.let{actionListener.goToSource(it)} }
+
+        this.setOnContextMenuRequested { contextMenuEvent ->
+            contextMenu.show(this, contextMenuEvent.screenX, contextMenuEvent.screenY)
+            contextMenuEvent.consume()
+        }
+    }
+}
