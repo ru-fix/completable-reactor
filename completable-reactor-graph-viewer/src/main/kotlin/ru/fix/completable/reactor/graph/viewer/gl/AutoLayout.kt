1475c3b4820b322f2442771360a98fd20aeaa0c0
kasfandiyarov <kasfandiyarov@fix.ru>
2018-02-05 10:30:45 +0300

    CR2: AutoLayout API.

--- completable-reactor-graph-viewer/src/main/kotlin/ru/fix/completable/reactor/graph/viewer/gl/AutoLayout.kt
+++ completable-reactor-graph-viewer/src/main/kotlin/ru/fix/completable/reactor/graph/viewer/gl/AutoLayout.kt
@@ -7,15 +7,21 @@ private val log = KotlinLogging.logger {}
 
 class AutoLayout {
     fun layout(node: AutoLayoutable) {
-        layout(listOf(node))
+        layout(listOf(node), HashSet())
     }
 
-    private tailrec fun layout(nodes: List<AutoLayoutable>) {
-        if (nodes.isEmpty()) {
-            return
-        }
+
+    private tailrec fun layout(nodes: List<AutoLayoutable>, visitedNodes: MutableSet<AutoLayoutable>) {
+
+        val children = ArrayList<AutoLayoutable>()
 
         for (node in nodes) {
+
+            if (visitedNodes.contains(node)) {
+                continue
+            }
+
+            visitedNodes.add(node)
 
             if (!node.isUserDefinedPosition) {
 
@@ -24,14 +30,20 @@ class AutoLayout {
                 when (figure) {
                     is StartPoint -> log.info { "Layout startPoint: ${figure}" }
                     is Handler -> log.info { "Layout handler: ${figure.name}" }
-                    is Merger -> log.info { "Layout handler: ${figure.name}" }
-                    is Router -> log.info { "Layout handler: ${figure.name}" }
-                    is Subgraph -> log.info { "Layout handler: ${figure.name}" }
+                    is Merger -> log.info { "Layout merger: ${figure.name}" }
+                    is Router -> log.info { "Layout router: ${figure.name}" }
+                    is Subgraph -> log.info { "Layout subgraph: ${figure.name}" }
                     is EndPoint -> log.info { "Layout endpoint: ${figure}" }
                 }
             }
+
+            children.addAll(node.graphChildren)
         }
 
-        layout(nodes.flatMap { it.graphChildren })
+        if (children.isEmpty()) {
+            return
+        }
+
+        layout(children, visitedNodes)
     }
 }
