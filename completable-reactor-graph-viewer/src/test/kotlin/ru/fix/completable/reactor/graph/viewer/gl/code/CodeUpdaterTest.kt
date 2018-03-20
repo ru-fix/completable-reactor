4d7a2a8e2d5e5a03e34e84653f469a9ea18c0e23
kasfandiyarov <kasfandiyarov@fix.ru>
2018-03-20 13:31:53 +0300

    Fix code updater to vx format

--- completable-reactor-graph-viewer/src/test/kotlin/ru/fix/completable/reactor/graph/viewer/gl/code/CodeUpdaterTest.kt
+++ completable-reactor-graph-viewer/src/test/kotlin/ru/fix/completable/reactor/graph/viewer/gl/code/CodeUpdaterTest.kt
@@ -10,8 +10,6 @@ import ru.fix.completable.reactor.graph.viewer.gl.code.CoordinateCodePhrase.*
  */
 class CodeUpdaterTest {
 
-    val updater = CodeUpdater()
-
     lateinit var codeCoordinates: List<CoordinateCodePhrase>
 
     fun normalize(value: String): String {
@@ -25,12 +23,10 @@ class CodeUpdaterTest {
     fun create_test_coordiate_items() {
 
         codeCoordinates = listOf(
-                StartPoint(500, -200),
-                Handler("smsRequestConstructProcessor4", 910, 901),
-                Handler("smsRequestConstructProcessor5", 1170, 1270),
-                Subgraph("userProfileSubgraph", 320, 1141),
-                Merger("serviceFetchProcessor5", 1210, 401),
-                EndPoint("smsRequestConstructProcessor6", 1500, 511))
+                StartPointPhrase(500, -200),
+                PlainVertexPhrase("plainVertex", 910, 901),
+                ComplexVertexPhrase("complexVertex", 1170, 1270, 123, 213),
+                EndPointPhrase("endPoint", 1500, 511))
     }
 
     private fun resourceAsText(resource: String): String {
@@ -38,11 +34,11 @@ class CodeUpdaterTest {
     }
 
     @Test
-    fun update_code_block() {
+    fun update_code_block_java() {
         val input = resourceAsText("/gl-code-block/code-block-1.txt")
         val expectedOutput = resourceAsText("/gl-code-block/code-block-1-result.txt")
 
-        val output = updater.updateCoordinates(input, codeCoordinates)
+        val output = CodeUpdater(CodeUpdater.CodeType.JAVA).updateCoordinates(input, codeCoordinates)
         Assert.assertEquals(normalize(expectedOutput), normalize(output))
     }
 
