f5c0cd937da5af5fbd1980ab6fa3e85ebbc47808
kasfandiyarov <kasfandiyarov@fix.ru>
2018-03-20 15:27:46 +0300

    Rename parser module

--- completable-reactor-graph/src/main/kotlin/ru/fix/completable/reactor/graph/internal/GlCoordinatesBuilder.kt
+++ completable-reactor-graph/src/main/kotlin/ru/fix/completable/reactor/graph/internal/GlCoordinatesBuilder.kt
@@ -35,4 +35,12 @@ class GlCoordinatesBuilder : CoordinatesBuilder {
     override fun vx(vertex: Vertex, x: Int, y: Int, x2: Int, y2: Int): CoordinatesBuilder {
         return this
     }
+
+    override fun pd(x: Int, y: Int): CoordinatesBuilder {
+        return this
+    }
+
+    override fun ct(vertex: Vertex, x: Int, y: Int): CoordinatesBuilder {
+        return this
+    }
 }
