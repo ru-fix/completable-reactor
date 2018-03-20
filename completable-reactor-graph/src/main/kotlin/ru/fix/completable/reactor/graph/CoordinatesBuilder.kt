7531668055791c74b49ac38910837a5c6c6d5278
kasfandiyarov <kasfandiyarov@fix.ru>
2018-03-20 09:49:28 +0300

    Fix coordinate builder

--- completable-reactor-graph/src/main/kotlin/ru/fix/completable/reactor/graph/CoordinatesBuilder.kt
+++ completable-reactor-graph/src/main/kotlin/ru/fix/completable/reactor/graph/CoordinatesBuilder.kt
@@ -6,17 +6,17 @@ interface CoordinatesBuilder {
      */
     fun payload(x: Int, y: Int): CoordinatesBuilder
 
-    fun handler(vertex: ru.fix.completable.reactor.graph.Vertex, x: Int, y: Int): CoordinatesBuilder
+    fun handler(vertex: Vertex, x: Int, y: Int): CoordinatesBuilder
 
-    fun merger(vertex: ru.fix.completable.reactor.graph.Vertex, x: Int, y: Int): CoordinatesBuilder
+    fun merger(vertex: Vertex, x: Int, y: Int): CoordinatesBuilder
 
-    fun router(vertex: ru.fix.completable.reactor.graph.Vertex, x: Int, y: Int): CoordinatesBuilder
+    fun router(vertex: Vertex, x: Int, y: Int): CoordinatesBuilder
 
-    fun subgraph(vertex: ru.fix.completable.reactor.graph.Vertex, x: Int, y: Int): CoordinatesBuilder
+    fun subgraph(vertex: Vertex, x: Int, y: Int): CoordinatesBuilder
 
-    fun vx(vertex: ru.fix.completable.reactor.graph.Vertex, x: Int, y: Int): CoordinatesBuilder
+    fun vx(vertex: Vertex, x: Int, y: Int): CoordinatesBuilder
 
-    fun vx(vertex: ru.fix.completable.reactor.graph.Vertex, x: Int, y: Int, x2: Int, y2: Int): CoordinatesBuilder
+    fun vx(vertex: Vertex, x: Int, y: Int, x2: Int, y2: Int): CoordinatesBuilder
 
-    fun complete(vertex: ru.fix.completable.reactor.graph.Vertex, x: Int, y: Int): CoordinatesBuilder
+    fun complete(vertex: Vertex, x: Int, y: Int): CoordinatesBuilder
 }
