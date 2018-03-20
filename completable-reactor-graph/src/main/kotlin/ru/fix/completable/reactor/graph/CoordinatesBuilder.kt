cca4e1cba19021ede3a0e9b836b4c97649bfe46e
kasfandiyarov <kasfandiyarov@fix.ru>
2018-03-20 15:14:39 +0300

    add short coordinates aliases

--- completable-reactor-graph/src/main/kotlin/ru/fix/completable/reactor/graph/CoordinatesBuilder.kt
+++ completable-reactor-graph/src/main/kotlin/ru/fix/completable/reactor/graph/CoordinatesBuilder.kt
@@ -1,22 +1,41 @@
 package ru.fix.completable.reactor.graph
 
 interface CoordinatesBuilder {
+
+    //TODO remove
+    fun payload(x: Int, y: Int): CoordinatesBuilder
     /**
      * Payload start point coordinates
      */
-    fun payload(x: Int, y: Int): CoordinatesBuilder
+    fun pd(x: Int, y: Int): CoordinatesBuilder
 
+    //TODO remove
     fun handler(vertex: Vertex, x: Int, y: Int): CoordinatesBuilder
 
+    //TODO remove
     fun merger(vertex: Vertex, x: Int, y: Int): CoordinatesBuilder
 
+    //TODO remove
     fun router(vertex: Vertex, x: Int, y: Int): CoordinatesBuilder
 
+    //TODO remove
     fun subgraph(vertex: Vertex, x: Int, y: Int): CoordinatesBuilder
 
+    /**
+     * Vertex coordinates
+     */
     fun vx(vertex: Vertex, x: Int, y: Int): CoordinatesBuilder
 
+    /**
+     * Vertex coordinates
+     */
     fun vx(vertex: Vertex, x: Int, y: Int, x2: Int, y2: Int): CoordinatesBuilder
 
+    //TODO remove
     fun complete(vertex: Vertex, x: Int, y: Int): CoordinatesBuilder
+
+    /**
+     * Complete coordinates
+     */
+    fun ct(vertex: Vertex, x: Int, y: Int): CoordinatesBuilder
 }
