fb3023476202e8b40d93d0122d69f1b6a0f2e2d1
kasfandiyarov <kasfandiyarov@gmail.com>
2018-04-10 11:18:58 +0300

    Restore Merger and mutator as Kotlin interfaces
    (cherry picked from commit 44c7fea2d1d286850957977d7cfe5cbff624f2a8)

--- completable-reactor-graph/src/main/java/ru/fix/completable/reactor/graph/Merger.java
+++
@@ -1,7 +0,0 @@
-package ru.fix.completable.reactor.graph;
-// Interface written in Java due to void returning value
-// Kotlin Unit type as returning value treated on Java side as Object
-@FunctionalInterface
-public interface Merger<Payload, HandlerResult> {
-    void merge(Payload payload, HandlerResult handlerResult);
-}
