923eae843857d3e6798730e6c8fd2496a0fd576d
kasfandiyarov <VkyabOvtTzp1821>
2018-08-06 19:22:34 +0300

    Start moving project to gradle.

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+rootProject.name = "completable-reactor"
+include("completable-reactor-example")
+include("completable-reactor-graph")
+include("completable-reactor-graph-kotlin")
+include("completable-reactor-graph-viewer")
+include("completable-reactor-graph-viewer-app")
+include("completable-reactor-model")
+include("completable-reactor-parser")
+include("completable-reactor-runtime")
+include("completable-reactor-spring")
+include("completable-reactor-ui")
