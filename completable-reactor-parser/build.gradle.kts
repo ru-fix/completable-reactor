60ca08096c67010170a6121d8589dc4fcb4763ea
kasfandiyarov <kasfandiyarov@fix.ru>
2018-09-03 15:15:22 +0300

    antlr parser generation task

--- completable-reactor-parser/build.gradle.kts
+++ completable-reactor-parser/build.gradle.kts
@@ -1,8 +1,11 @@
 import org.gradle.kotlin.dsl.*
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     java
     kotlin("jvm")
+
+    id("antlr")
 }
 
 dependencies {
@@ -14,10 +17,46 @@ dependencies {
     compile(Libs.kotlin_reflect)
     compile(Libs.kotlin_logging)
 
-    compile(Libs.antlr)
+    compile(Libs.antlrRuntime)
+
+    antlr(Libs.antlrAll)
 
 
     testImplementation(Libs.junit_api)
     testRuntimeOnly(Libs.junit_engine)
     testRuntimeOnly(Libs.slf4j_simple)
 }
+
+val antlrDestinationDir = "src/main/java/ru/fix/completable/reactor/parser/java/antlr"
+
+tasks.register("generateAntlr"){
+    description = """
+        Generates java source from antlr
+        Antlr source generated and replaced everything within $antlrDestinationDir
+        Antlr task generation is disabled by default since antlr g4 files changes rarely
+        and it tooks time to generate java code.
+    """.trimIndent()
+    doLast{
+        tasks.withType<AntlrTask>{
+            enabled = true
+            includes.add("Graph.g4")
+            arguments.addAll(listOf(
+                    "-no-visitor",
+                    "-no-listener",
+                    "-package",
+                    "ru.fix.completable.reactor.parser.java.antlr"
+            ))
+            outputDirectory = project.projectDir.resolve(antlrDestinationDir)
+
+            execute()
+        }
+    }
+}
+
+tasks {
+    withType<AntlrTask> {
+        enabled = false
+    }
+}
+
+
