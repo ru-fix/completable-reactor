62bf932744ed5bc2752b2ae93ae1b2fb5bcd513e
kasfandiyarov <kasfandiyarov@fix.ru>
2018-10-23 17:14:49 +0300

    Add todo for docs

--- completable-reactor-graph/src/main/kotlin/ru/fix/completable/reactor/graph/Router.kt
+++ completable-reactor-graph/src/main/kotlin/ru/fix/completable/reactor/graph/Router.kt
@@ -2,5 +2,6 @@ package ru.fix.completable.reactor.graph
 
 @FunctionalInterface
 interface Router<Payload> {
+    //TODO: add documentation to all public interfaces
     fun route(payload: Payload): Enum<*>
 }
